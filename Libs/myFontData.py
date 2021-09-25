a244791f84b7f128ad97461e8b3ae2c05c910755
HN410 <HN410>
2021-09-25 12:16:59 +0900

    add myFontData

---
+++ Libs/myFontData.py
@@ -0,0 +1,24 @@
+import random
+from .myFontLib import *
+import torch.utils.data as data
+class FontGeneratorDataset(data.Dataset):
+    # ゴシック体と各フォントのペア画像の組を出力するデータセット
+
+    def __init__(self, fontTools: FontTools, compatibleDict: dict, imageN : list):
+        #  fontTools ... FontTools
+        #  compatibleDict ... 各フォントごとに対応している文字のリストを紐づけたディクショナリ
+        #  imageN ... ペア画像を出力する数の範囲(要素は２つ)
+        #             (例) [3, 6] ... 3~6個の中から一様分布で決定される
+        #                  [4, 4] ...4個で固定
+        self.fontTools = fontTools
+        self.fontList = fontTools.getFontPathList()
+        self.compatibleDict = compatibleDict
+        self.imageN = imageN
+    def __len__(self):
+        return len(self.fontList)
+    
+    def __getitem__(self, index):
+        charaChooser = CharacterChooser(self.fontTools, self.fontList[index],
+                 self.compatibleDict[index])
+        sampleN = random.randint(self.imageN[0], self.imageN[1])
+        return charaChooser.getSampledImagePair(sampleN)
