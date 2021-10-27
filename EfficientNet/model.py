cc87170ad1829c7d6d7e8fafccef94fc187aa16f
HN410 <HN410>
2021-10-27 22:43:08 +0900

    debug

--- EfficientNet/model.py
+++ EfficientNet/model.py
@@ -384,7 +384,7 @@ class EfficientNetEncoder(nn.Module):
             x = block(x, drop_connect_rate=drop_connect_rate)
             if self.ver < 2 and (idx in self.used_maps_indices):
                 used_maps.append(x) # 場合によってはclone()すべき？
-        if self.ver >= 2 and self.isForCharacter:
+        if self.ver >= 2 :
             # [B, 320, 8, 8]
             return x
         # Upsample
