7784f145583cf1024def47d3cb146bf1b58d6bfe
HN410 <HN410>
2021-11-26 10:10:14 +0900

    add d_wgan_loss2

--- Libs/myLoss.py
+++ Libs/myLoss.py
@@ -1,6 +1,6 @@
 import torch
 from torch.nn import functional as F
-
+import numpy as np
 
 def d_lsgan_loss(discriminator, trues, fakes, labels, alpha):
     d_trues = discriminator.forward(trues, labels, alpha)
@@ -17,7 +17,7 @@ def g_lsgan_loss(discriminator, fakes, labels, alpha):
     return loss
 
 
-def d_wgan_loss(discriminator, d_trues, d_fakes, trues, fakes,  teachers, alpha, phase, useGradient = True):
+def d_wgan_loss(discriminator, d_trues, d_fakes, before,  trues, fakes,  teachers, alpha, phase, useGradient = True):
     epsilon_drift = 1e-3
     lambda_gp = 10
 
@@ -31,8 +31,9 @@ def d_wgan_loss(discriminator, d_trues, d_fakes, trues, fakes,  teachers, alpha,
         epsilon = torch.rand(batch_size, 1, 1, 1, dtype=fakes.dtype, device=fakes.device)
         intpl = epsilon * fakes + (1 - epsilon) * trues
         intpl.requires_grad_()
-        f = discriminator.forward( intpl, teachers,  alpha)
+        f = discriminator.forward(before, intpl, teachers,  alpha)
         grad = torch.autograd.grad(f.sum(), intpl, create_graph=True)[0]
+        del intpl, epsilon, f
         grad_norm = grad.view(batch_size, -1).norm(dim=1)
         loss_gp = lambda_gp * ((grad_norm - 1) ** 2).mean()
 
@@ -45,9 +46,55 @@ def d_wgan_loss(discriminator, d_trues, d_fakes, trues, fakes,  teachers, alpha,
 
     return loss, wd
 
+def d_wgan_loss2(discriminator,  before,  trues, fakes,  teachers, alpha, phase, useGradient = True):
+    epsilon_drift = 1e-3
+    lambda_gp = 10
+    loss_list = []
+
+    batch_size = fakes.size()[0]
+
+    d_trues = discriminator(before, trues, teachers, alpha)
+    d_fakes = discriminator(before, fakes, teachers, alpha)
+
+    loss_wd =  torch.nn.LeakyReLU(0.002)(1- d_trues).mean() +\
+         torch.nn.LeakyReLU(0.002)(1 + d_fakes).mean()
+    correctN = (d_trues > 0).sum().item() + (d_fakes <= 0).sum().item()
+    loss_list.append(loss_wd.item())
+
+    # drift
+    loss_wd += epsilon_drift * (d_trues ** 2).mean()
+    # loss_wd += epsilon_drift * (d_fakes ** 2).mean() # 不安定なため追加
+
+    del d_fakes, d_trues
+    loss_list.append(loss_wd.item() - loss_list[0])
+
+    # gradient penalty
+    loss_gp = 0
+    if(phase == "train" and useGradient):
+        epsilon = torch.rand(batch_size, 1, 1, 1, dtype=fakes.dtype, device=fakes.device)
+        intpl = epsilon * fakes + (1 - epsilon) * trues
+        intpl.requires_grad_()
+        f = discriminator.forward(before, intpl, teachers,  alpha)
+        grad = torch.autograd.grad(f.sum(), intpl, create_graph=True)[0]
+        del intpl, epsilon, f
+        grad_norm = grad.view(batch_size, -1).norm(dim=1)
+        loss_gp = lambda_gp * ((grad_norm - 1) ** 2).mean()
+        loss_list.append(loss_gp.item())
+    else:
+        loss_list.append(loss_gp)
+
+
+    loss = loss_wd  + loss_gp
+
+    
+    # wd = loss_wd.item()
+
+    return loss, correctN, np.array(loss_list)
+
+
 
 def g_wgan_loss(d_fakes):
-    loss = -d_fakes.mean()
+    loss = torch.nn.LeakyReLU(0.002)(1 -d_fakes).mean()
     return loss
 
 
@@ -67,3 +114,4 @@ def d_logistic_loss(discriminator, trues, fakes, labels, alpha, r1gamma=10):
 def g_logistic_loss(discriminator, fakes, labels, alpha):
     d_fakes = discriminator.forward(fakes, labels, alpha)
     return F.softplus(-d_fakes).mean()
+
